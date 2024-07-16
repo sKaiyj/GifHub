8b0e3ac9b8f4c4464caa401decb5adad2ebfb97f
sKaiyj <dnionin@gmail.com>
2024-07-16 22:56:16 +0300

    working with SDK, but not as i want

--- src/components/contentLine/ContentLine.tsx
+++ src/components/contentLine/ContentLine.tsx
@@ -3,14 +3,19 @@ import { Grid } from "@giphy/react-components";
 import { GiphyFetch } from "@giphy/js-fetch-api";
 import React from "react";
 import { useSelector } from "react-redux";
+import { useLocation } from "react-router-dom";
 
 const ContentLine = () => {
-  const gf = new GiphyFetch("TfEf3OStkFOrSCIPUitewGJjIifsNhQB");
+  const gf = new GiphyFetch("TfEf3OStkFOrSCIPUitewGJjIifsNhQB"); // убрать отдельно
+  const location = useLocation().pathname;
   const searchNow = useSelector((state: any) => state.searchNow);
 
-  let fetchGifs = (offset: number) =>
-    gf.search(searchNow, { offset, limit: 10 });
-
+  let fetchGifs: any;
+  if (location === "/") {
+    fetchGifs = (offset: number) => gf.search(searchNow, { offset, limit: 10 });
+  } else {
+    fetchGifs = (offset: number) => gf.trending({ limit: 10, offset });
+  }
   return (
     <div className={styles.contentLine}>
       <Grid
--- src/pages/randomPage/RandomPage.tsx
+++ src/pages/randomPage/RandomPage.tsx
@@ -1,10 +1,33 @@
-import Search from "../../components/search/Search";
-import ContentLine from "../../components/contentLine/ContentLine";
+import React, { useEffect } from "react";
+import style from "./RandomPage.module.scss";
+import { Gif } from "@giphy/react-components";
+import { GiphyFetch } from "@giphy/js-fetch-api";
+import CustomButton from "../../components/button/CustomButton";
 
 function RandomPage() {
+  const gf = new GiphyFetch("Mg9uQkfKo8mLERCRJRzPaN5qZoCQWSBc"); // убрать отдельно
+  const [newRandom, setNewRandom] = React.useState(false);
+  const [asyncData, setAsyncData] = React.useState<any>(null!);
+  useEffect(() => {
+    gf.random().then((res) => setAsyncData(res));
+    console.log(asyncData);
+  }, [newRandom]);
+
+  const onClick = () => {
+    fetch(
+      "https://api.giphy.com/v1/gifs/random?api_key=gVCqGpoOmqqoc38CuLQLodl2lJNA3HQs"
+    ).then((res) => console.log(res.json()));
+  };
   return (
-    <div className='RandomPage'>
-      <ContentLine />
+    <div className={style.randomPage}>
+      {asyncData ? (
+        <>
+          <Gif gif={asyncData.data} width={400} className={style.gif} />
+          <CustomButton onClick={onClick}>new random</CustomButton>
+        </>
+      ) : (
+        <div>Loading...</div>
+      )}
     </div>
   );
 }
